7193d327a9fe950720a1fd12a58b54542101bb72
ruby -v <anastasia.betsa@gmail.com>
2017-11-28 20:54:40 +0200

    name

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
+  has_many :postcomments
+  has_many :photos
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/default.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  validates :nickname,:email,:password,:password_confirmation, presence: true
+  validates :nickname, length: { in: 2..15 }
+  validates :nickname, length: { in: 6..20 }
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  root to: "pages#index"
+
+  resources :posts do
+  	resources :postcomments
+  end	
+
+  resources :photos
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
